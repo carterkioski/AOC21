44666be29d6fdce2a91e9cdc4b7b0ba98c7e4cfa
carterkioski <carterkioski@gmail.com>
2021-12-01 20:19:50 -0800

    cleaning up day 1 part 1 , completed part 2 using map/zip

--- code/day1.py
+++ code/day1.py
@@ -1,11 +1,23 @@
+#part 1
 increases = 0
 prev = 0
 with open('../inputs/day1.txt', 'r') as f:
-    for i in f.readlines():
-    	if not prev:
-    		prev = int(i)
-    		continue
+    for i in f.readlines()[1:]:
     	if int(i) > prev:
     		increases += 1
     	prev = int(i)
+print(increases)
+
+#part 2
+increases = 0
+prev = 0
+with open('../inputs/day1.txt', 'r') as f:
+    lines = list(map(int,f.readlines()))
+    a = zip(lines, lines[1:], lines[2:])
+    for i in a:
+        if prev:
+            increases += sum(i) > prev
+        prev = sum(i)
+        
+    	
 print(increases)
