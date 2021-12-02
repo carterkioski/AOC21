a565ab75244fe4534a8513e9b6e30cc29333bfc6
carterkioski <carterkioski@gmail.com>
2021-12-01 21:16:26 -0800

    day 2 both parts done - needs cleaning up

--- code/day2.py
+++ code/day2.py
@@ -1,7 +1,32 @@
 #part 1
-#with open('../inputs/test.txt', 'r') as f:
+x = 0
+y = 0
+with open('../inputs/day2.txt', 'r') as f:
+	directions = [t.split() for t in f.readlines()]
+	for direction in directions:
+		if direction[0] == 'forward':
+			x += int(direction[1])
+		elif direction[0] == 'up':
+			y += int(direction[1])
+		elif direction[0] == 'down':
+			y -= int(direction[1]) 
+
+print(abs(x * y))
 
 
 
 #part 2    
-#with open('../inputs/test.txt', 'r') as f:
+x = 0
+y = 0
+aim = 0
+with open('../inputs/day2.txt', 'r') as f:
+	directions = [t.split() for t in f.readlines()]
+	for direction in directions:
+		if direction[0] == 'forward':
+			x += int(direction[1])
+			y += int(direction[1]) * aim
+		elif direction[0] == 'up':
+			aim -= int(direction[1])
+		elif direction[0] == 'down':
+			aim += int(direction[1]) 
+print(abs(x * y))
