1e388d74bb026693832583c1354f7fddaed596fb
carterkioski <carterkioski@gmail.com>
2021-12-04 11:59:44 -0800

    finishing part 2 day 3

--- code/day3.py
+++ code/day3.py
@@ -1,5 +1,6 @@
+import re
 #part 1
-with open('../inputs/test.txt', 'r') as f:
+'''with open('../inputs/test.txt', 'r') as f:
 	lines = f.readlines()
 	total = len(lines)
 	#will only count the 1s in each 'column'
@@ -10,13 +11,48 @@ with open('../inputs/test.txt', 'r') as f:
 				a[i] = a[i]+1
 	gamma = ''.join(['1' if num > total/2 else '0' for num in a])
 	epsilon = ''.join(['1' if num < total/2 else '0' for num in a])
-	#epsilon = [1 if num == 0 else 0 for num in gamma]
 	i_gamma = int(gamma, 2)
 	i_epsilon = int(epsilon,2)
 	print(i_epsilon * i_gamma)
-
+'''
 
 
 
 #part 2    
-#with open('../inputs/test.txt', 'r') as f:
+with open('../inputs/day3.txt', 'r') as f:
+	lines = [line.strip() for line in f.readlines()]
+item_len = len(lines[0])
+o_lines = list(lines)
+c_lines = list(lines)
+oxygen = list()
+carbon = list()
+#will only count the 1s in each 'column'
+def find_count(binaries, pos, type):
+	count = 0
+	total = len(binaries)
+	for line in binaries:
+		if int(line[pos]):
+			count += 1
+	if type == 'o':
+		return '1' if count >= total/2 else '0'
+	else:
+		return '1' if count < total/2 else '0'
+
+
+
+for i in range(0,item_len):
+	if len(o_lines) > 1:
+		oxygen.append(find_count(o_lines,i,'o'))
+		o_r = re.compile(f'{"".join(oxygen)}.*')
+		o_lines = list(filter(o_r.match,o_lines))
+	else:
+		oxygen = o_lines[0]
+
+	if len(c_lines) > 1:
+		carbon.append(find_count(c_lines,i,'c'))
+		c_r = re.compile(f'{"".join(carbon)}')
+		c_lines = list(filter(c_r.match,c_lines))
+	else:
+		carbon = c_lines[0]
+
+print(int(''.join(oxygen),2) * int(''.join(carbon),2))
